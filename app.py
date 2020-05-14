7086f670c0c47fa127dcd222e3c567c182e71d5b
Daniel Thompson <DanThomp507@gmail.com>
2020-05-14 16:59:48 -0400

    update

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ PAT="EAAg5QDZAOmyEBACR7v7a3hrKtKhsSq7X6XIq5cZAEncGz4cSfYXMNsWrKI2PRboedmJZBgfsSj
 @app.route('/', methods=['GET'])
 def handle_verification():
   print('Handling Verification')
+  print(request.args.get('hab.verify_token'))
   if request.args.get('hub.verify_token', '') == 'my_voice_is_my_password_verify_me':
     print('Verification successful!')
     return request.args.get('hub.challenge', '')
