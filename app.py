e4b778b1d10e6834fbe50c2e809879ee2698016f
Daniel Thompson <DanThomp507@gmail.com>
2020-05-14 16:44:54 -0400

    fixed linting error

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 # app.config.from_pyfile('settings.py')
 app.config['PAT'] = environ.get('PAT')
 
+
 @app.route('/', methods=['GET'])
 def handle_verification():
     print('Handling Verification')
@@ -16,6 +17,7 @@ def handle_verification():
     else:
         print('Verification Failed!')
         return 'Error, wrong validation token!'
+
 
 @app.route('/', methods=['POST'])
 def handle_messages():
@@ -27,6 +29,7 @@ def handle_messages():
         send_message(app.config['PAT'], sender, message)
         return "Ok"
 
+
 def messaging_events(payload):
     """Generate tuples of (sender_id, message_text)
 from the provided payload
@@ -35,24 +38,38 @@ from the provided payload
     messaging_events = data['entry'][0]['messaging']
     for event in messaging_events:
         if 'message' in event and 'text' in event['message']:
-            yield event['sender']['id'], 
+            yield event['sender']['id'],
             event['message']['text'].encode('unicode_escape')
         else:
-            yield event ['sender']['id'], 'I cannot echo this'
+            yield event['sender']['id'], 'I cannot echo this'
 
+
+# def send_message(token, recipient, text):
+#     """Send the message text to recipient with id recipient.
+#     """
+
+# r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+#         params={"access_token": token },
+#         data=json.dumps({
+#       "recipient": {"id": recipient },
+#       "message": {"text": text.decode('unicode_escape')}
+#     }),
+#     headers={'Content-type': 'application/json'})
+# if r.status_code != requests.codes.ok: #pylint: disable=no-member
+#         print(r.text)
 def send_message(token, recipient, text):
-      """Send the message text to recipient with id recipient.
-  """
+    """Send the message text to recipient with id recipient.
+    """
 
-  r = requests.post("https://graph.facebook.com/v2.6/me/messages",
-    params={"access_token": token},
-    data=json.dumps({
-      "recipient": {"id": recipient},
-      "message": {"text": text.decode('unicode_escape')}
-    }),
-    headers={'Content-type': 'application/json'})
-  if r.status_code != requests.codes.ok:
-    print r.text
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+        params={"access_token": token},
+        data=json.dumps({
+            "recipient": {"id": recipient},
+            "message": {"text": text.decode('unicode_escape')}
+        }),
+        headers={'Content-type': 'application/json'})
+    if r.status_code != requests.codes.ok:
+        print(r.text)
 
 if __name__ == '__main__':
     app.run()
