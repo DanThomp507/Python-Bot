48ef38a0221b2310e924650b736144e4534f7db9
Daniel Thompson <DanThomp507@gmail.com>
2020-05-14 22:05:03 -0400

    clean up

--- app.py
+++ app.py
@@ -7,13 +7,9 @@ from datetime import datetime
 
 app = Flask(__name__)
 PAT = "EAAg5QDZAOmyEBAHxw7C1BpUZCl7uq1iZCEZAAGgYzrYuO5HnmZAdziKdcMLhZCyj2jT1dVq0d7yIQE988oW6t4F5fd8Q96PAN70EpiNQ4HdWm7lBUYbcBwYMQbXHFGrIWjC6kpaxTpiob855AwFQuDlntZA9FOIaIrcMfPNc27UBAZDZD"
-# app.config.from_pyfile('settings.py')
-# app.config['PAT'] = environ.get('PAT')
 
 @app.route('/', methods=['GET'])
 def verify():
-    # when the endpoint is registered as a webhook, it must echo back
-    # the 'hub.challenge' value it receives in the query arguments
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == PAT:
             return "Verification token mismatch", 403
@@ -21,87 +17,32 @@ def verify():
 
     return "Hello world", 200
 
-
-# @app.route('/', methods=['POST'])
-# def handle_messages():
-#     print('Handling Messages...')
-#     payload = request.get_data()
-#     print(payload)
-#     for sender, message in messaging_events(payload):
-#         print('Incoming from %s: %s' % (sender, message))
-#         send_message(PAT, sender, message)
-#         return "Ok"
-
-
-# def messaging_events(payload):
-#     """Generate tuples of (sender_id, message_text)
-# from the provided payload
-#     """
-#     data = json.loads(payload)
-#     messaging_events = data['entry'][0]['messaging']
-#     for event in messaging_events:
-#         if 'message' in event and 'text' in event['message']:
-#             yield event['sender']['id'],
-#             event['message']['text'].encode('unicode_escape')
-#         else:
-#             yield event['sender']['id'], 'I cannot echo this'
-
-
-# # def send_message(token, recipient, text):
-# #     """Send the message text to recipient with id recipient.
-# #     """
-
-# # r = requests.post("https://graph.facebook.com/v2.6/me/messages",
-# #         params={"access_token": token },
-# #         data=json.dumps({
-# #       "recipient": {"id": recipient },
-# #       "message": {"text": text.decode('unicode_escape')}
-# #     }),
-# #     headers={'Content-type': 'application/json'})
-# # if r.status_code != requests.codes.ok: #pylint: disable=no-member
-# #         print(r.text)
-# def send_message(token, recipient, text):
-#     """Send the message text to recipient with id recipient.
-#     """
-
-#     r = requests.post("https://graph.facebook.com/v2.6/me/messages",
-#         params={"access_token": token},
-#         data=json.dumps({
-#             "recipient": {"id": recipient},
-#             "message": {"text": text.decode('unicode_escape')}
-#         }),
-#         headers={'Content-type': 'application/json'})
-#     if r.status_code != requests.codes.ok:
-#         print(r.text)
-
 @app.route('/', methods=['POST'])
 def webhook():
 
-    # endpoint for processing incoming messaging events
-
     data = request.get_json()
-    print(data)  # you may not want to log every incoming message in production, but it's good for testing
+    print(data)
 
     if data["object"] == "page":
 
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
 
-                if messaging_event.get("message"):  # someone sent us a message
+                if messaging_event.get("message"): 
 
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+                    sender_id = messaging_event["sender"]["id"]       
+                    recipient_id = messaging_event["recipient"]["id"] 
+                    message_text = messaging_event["message"]["text"] 
 
                     send_message(sender_id, "roger that!")
 
-                if messaging_event.get("delivery"):  # delivery confirmation
+                if messaging_event.get("delivery"):
                     pass
 
-                if messaging_event.get("optin"):  # optin confirmation
+                if messaging_event.get("optin"):
                     pass
 
-                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                if messaging_event.get("postback"):
                     pass
 
     return "ok", 200
